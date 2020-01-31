d743870bd0b1aeaebe313af1f484998678606865
TheMindset <mulezig2014@outlook.fr>
2020-01-31 15:03:36 +0100

    Fetch data from moviedb API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { getMovies } from '../utils/apiCalls'
+import { setMovies } from '../actions'
 
 class App extends Component {
+
+  async componentDidMount() {
+    const { setMovies } = this.props
+    try {
+      const data = await getMovies()
+      setMovies(data)
+    } catch(error) {
+      console.log(error.message)
+    }
+  }
 
   render() {
     return (
@@ -12,4 +25,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  setMovies: (movies) => dispatch(setMovies(movies))
+})
+
+export default connect(null, mapDispatchToProps)(App)
