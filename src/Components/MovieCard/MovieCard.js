18c639888b6718138939f0b1148d065110b66cec
TheMindset <mulezig2014@outlook.fr>
2020-02-01 19:26:57 +0100

    Movie card style & animation

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -4,10 +4,10 @@ import React from 'react'
 const MovieCard = ({movie}) => {
   return (
     <div className='movie'>
+      <img className='card-image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
       <h2 className='card-header'>
         {movie.title}
       </h2>
-      <img className='card-image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
       <h4 className='card-release-date'>
         {movie.release_date.slice(0, 4)}
       </h4>
