7de16bb5d1b34f2631b052ce63948e6235ef364f
TheMindset <mulezig2014@outlook.fr>
2020-02-02 07:00:31 +0100

    Add favorite icon to movieCard

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import star from '../../images/red-star.svg'
 
 
 const MovieCard = ({movie}) => {
   return (
     <div className='movie'>
+      <img src={star} className='card-favorite-image' alt='star indicate if the movie is favorite or not' />
+
       <img className='card-image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
-      <h2 className='card-header'>
-        {movie.title}
-      </h2>
+      <div className='card-header'>
+        <p>
+          {movie.title}
+        </p>
+      </div>
       <h4 className='card-release-date'>
         {movie.release_date.slice(0, 4)}
       </h4>
