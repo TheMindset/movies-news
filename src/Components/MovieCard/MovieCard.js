95ee4c57c45e3ae0057010e23558c882e68697b4
TheMindset <mulezig2014@outlook.fr>
2020-02-04 11:53:56 +0100

    Add  conditional logic for display favorites movies

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import star from '../../images/red-star.svg'
+import favStar from '../../images/fav-star.svg'
 
-
-const MovieCard = ({ movie, toggleFavorites }) => {
+const MovieCard = ({ movie, toggleFavorites, favorites }) => {
+  const isFavorite = favorites.map((favMovie) => favMovie.title).includes(movie.title)
+  const favImg = isFavorite ? favStar : star;
+  const favClass = isFavorite ? 'favorite': ''
   const id = movie.movie_id
   return (
     <Link to={`/movie/${id}`}>
-      <div className='movie' movie_id={movie.movie_id}>
-        <img src={star} 
+      <div className={`movie ${favClass}`} movie_id={movie.movie_id}>
+        <img src={favImg} 
           className='card-favorite-image' 
           alt='star indicate if the movie is favorite or not' 
           onClick={ (event) =>  {
@@ -35,4 +39,8 @@ const MovieCard = ({ movie, toggleFavorites }) => {
   )
 }
 
-export default MovieCard
+const mapStateToProps = (state) => ({
+  favorites: state.favorites
+})
+
+export default  connect(mapStateToProps)(MovieCard)
